5facaa39d9d986b3b669c309b73a05902e5d37f8
ivangotovets <gotovets@gmail.com>
2023-01-24 22:34:40 +0300

    add exception handling

--- kittybot.py
+++ kittybot.py
@@ -1,30 +1,50 @@
+from dotenv import load_dotenv
+import os
+import requests
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import Updater, Filters, MessageHandler, CommandHandler
 
-updater = Updater(token='5829159779:AAHdFKHv34HKT3f1UwnUR0xlhY0U3jRZstE')
+CHAT_ID = 244065258
+TEXT = 'Привет-привет'
+URL = 'https://api.thecatapi.com/v1/images/search'
+
+load_dotenv()
+token = os.getenv('TOKEN')
+
+updater = Updater(token=token)
 
 
-def say_hi(update, context):
+def get_new_image():
+    try:
+        response = requests.get(URL)
+    except Exception as e:
+        print(e)
+        new_url = 'https://api.thedogapi.com/v1/images/search'
+        response = requests.get(new_url)
+
+    response = response.json()
+    random_cat = response[0].get('url')
+    return random_cat
+
+
+def new_cat(update, context):
     chat = update.effective_chat
-    context.bot.send_message(chat_id=chat.id, text='Привет, я KittyBot!')
+    context.bot.send_photo(chat.id, get_new_image())
 
 
 def wake_up(update, context):
     chat = update.effective_chat
     name = update.message.chat.first_name
-    buttons = ReplyKeyboardMarkup([
-        ['Определи IP', 'Который час'],
-        ['/random_digit'],
-    ])
+    buttons = ReplyKeyboardMarkup([['/newcat']], resize_keyboard=True)
     context.bot.send_message(
         chat_id=chat.id,
-        text='Спасибо, что включил, {}'.format(name),
+        text='Привет, {}, смотри какие котаны!'.format(name),
         reply_markup=buttons,
     )
 
 
 updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, say_hi))
+updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
 
-updater.start_polling(poll_interval=20.0)
+updater.start_polling()
 updater.idle()
